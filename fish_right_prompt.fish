504a57e80d0a3f155664f95b5925700194758c7e
Joel Cornett <joel.cornett@gmail.com>
2017-02-08 11:02:08 -0500

    add fish_right_prompt

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,3 +1,18 @@
+function _git_branch
+  command git rev-parse --abbrev-ref HEAD
+end
+function _git_dirty
+  test -n (command git status -s -uno ^/dev/null)
+end
 function fish_right_prompt
-  # Customize the right prompt
+  set -l git_branch (_git_branch)
+  if test $status -eq 0
+    echo -n (set_color normal)'('
+    if _git_dirty
+      echo -n (set_color ff6c5d red)
+    else
+      echo -n (set_color 6291ff white)
+    end
+    echo -n $git_branch(set_color normal)')'
+  end
 end
