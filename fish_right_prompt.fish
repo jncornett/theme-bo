95c5e5ace7e15afd72be07a50e74ec362be6c4e2
Joel Cornett <joel.cornett@gmail.com>
2017-02-08 12:06:28 -0500

    fix for empty argument to test

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -12,18 +12,23 @@ function _git_dirty
 end
 function fish_right_prompt
   set -l exit_code $status
-  if test "$exit_code" -ne 0
-    echo -n (set_color ff6c5d red)'⏎ '
+  if [ $exit_code ]
+    if test "$exit_code" -ne 0
+      echo -n (set_color ff6c5d red)'⏎ '
+    end
   end
   echo -n (set_color brblack)'('
   set -l git_branch (_git_branch)
-  if test "$status" -eq 0
-    if _git_dirty
-      echo -n (set_color ff6c5d red)
-    else
-      echo -n (set_color 6291ff white)
+  set -l exit_code $status
+  if [ $exit_code ]
+    if test "$exit_code" -eq 0
+      if _git_dirty
+        echo -n (set_color ff6c5d red)
+      else
+        echo -n (set_color 6291ff white)
+      end
+      echo -n $git_branch(set_color brblack)':'
     end
-    echo -n $git_branch(set_color brblack)':'
   end
   echo -n (set_color 6291ff white)(prompt_pwd)(set_color brblack)')'
 end
