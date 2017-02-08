26b850b9d82646e367b957300de8f7e8e59e4b1c
Joel Cornett <joel.cornett@gmail.com>
2017-02-08 11:12:44 -0500

    add fish_right_prompt

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,5 +1,5 @@
 function _git_branch
-  command git rev-parse --abbrev-ref HEAD
+  command git rev-parse --abbrev-ref HEAD ^/dev/null
 end
 function _git_dirty
   set -l git_status (command git status -s -uno ^/dev/null)
@@ -11,14 +11,19 @@ function _git_dirty
   return 1
 end
 function fish_right_prompt
+  set -l exit_code $status
+  if test $exit_code -ne 0
+    echo -n (set_color ff6c5d red)'⏎ '
+  end
+  echo -n (set_color brblack)'('
   set -l git_branch (_git_branch)
   if test $status -eq 0
-    echo -n (set_color normal)'('
     if _git_dirty
       echo -n (set_color ff6c5d red)
     else
       echo -n (set_color 6291ff white)
     end
-    echo -n $git_branch(set_color normal)')'
+    echo -n $git_branch(set_color brblack)':'
   end
+  echo -n (set_color 6291ff white)(prompt_pwd)(set_color brblack)')'
 end
