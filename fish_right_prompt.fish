f748044f8c7b3f3d67d1ea3f6e651871244b4a7f
Joel Cornett <joel.cornett@gmail.com>
2017-02-08 12:23:50 -0500

    fix for empty argument to test

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,34 +1,33 @@
-function _git_branch
-  command git rev-parse --abbrev-ref HEAD ^/dev/null
-end
 function _git_dirty
-  set -l git_status (command git status -s -uno ^/dev/null)
-  if test "$status" -eq 0
-    if [ $git_status ]
-      return 0
-    end
+  set -l status_info (command git status -s -uno ^/dev/null)
+  [ $status = 0 ]; or return 1
+  [ $status_info ]
+end
+function _git_status
+  set -l branch_name (command git rev-parse --abbrev-ref HEAD ^/dev/null)
+  test $status -ne 0; and return  # not in a git repo
+  if _git_dirty
+    set_color ff6c5d red
+  else
+    set_color 6291ff white
   end
-  return 1
+  echo -n $branch_name
+  set_color brblack
+  echo -n ':'
+end
+function _exit_code
+  set -l exit_code $status
+  if test "$exit_code" -ne 0
+    echo -n (set_color ff6c5d red)'⏎ '
+  end
 end
 function fish_right_prompt
-  set -l exit_code $status
-  if [ $exit_code ]
-    if test "$exit_code" -ne 0
-      echo -n (set_color ff6c5d red)'⏎ '
-    end
-  end
-  echo -n (set_color brblack)'('
-  set -l git_branch (_git_branch)
-  set -l exit_code $status
-  if [ $exit_code ]
-    if test "$exit_code" -eq 0
-      if _git_dirty
-        echo -n (set_color ff6c5d red)
-      else
-        echo -n (set_color 6291ff white)
-      end
-      echo -n $git_branch(set_color brblack)':'
-    end
-  end
-  echo -n (set_color 6291ff white)(prompt_pwd)(set_color brblack)')'
+  _exit_code
+  set_color brblack
+  echo -n '('
+  _git_status
+  set_color 6291ff white
+  echo -n (prompt_pwd)
+  set_color brblack
+  echo -n ')'
 end
